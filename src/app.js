2d006a227df79d65cc0fb3f8a9be1e2e464570ba
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-08-31 19:31:52 -0500

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 //* Dependencias
 const express = require("express");
 const passport = require("passport");
+const { uploadCovers, uploadChapters } = require("./utils/multer");
 require("./middleware/auth.middleware")(passport);
 
 //*Archivos de rutas
 const userRouter = require("./users/users.router").router;
 const authRouter = require("./auth/auth.router").router;
+const programsRouter = require("./programs/programs.router").router;
 
 //* Configuraciones iniciales
 const app = express();
+const router = express.Router();
 
 //? Esta configuracion es para habilitar el req.body
 app.use(express.json());
@@ -17,13 +20,24 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "All ok!" });
 });
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/auth", authRouter);
+
+app.post('/prueba-covers', uploadCovers.single('cover-img'), (req, res) => {
+  const { title, description } = req.body;
+  res.status(200).json({ message: req.file.filename, title, description });
+});
+
+app.post('/prueba-chapters', uploadChapters.single('chapter-video'), (req, res) => {
+  const { program_id, chapter_num } = req.body;
+  res.status(200).json({ message: req.file.filename, program_id, chapter_num });
+});
+
+app.use("/api/v1", router);
+router.use("/users", userRouter);
+router.use("/auth", authRouter);
+router.use("/programs", programsRouter);
 
 app.listen(8000, () => {
   console.log("Server started at port 8000");
 });
 
 exports.default = app
-exports.app = app
-module.exports = app
---
+++ src/chapters/chapters.http.js
@@ -0,0 +1,105 @@
+const chaptersControllers = require('./chapters.controllers');
+const programsControllers = require('./../programs/programs.controller');
+
+const getChapters = (req, res) => {
+    try {
+        const { program_id } = req.params;
+        const program = programsControllers.getProgramById(program_id);
+        const chapters = chaptersControllers.getChaptersByProgram(program_id);
+
+        res.status(200).json({ ...program, chapters });
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message });
+    }
+};
+
+const create = (req, res) => {
+    try {
+        const { program_id } = req.params;
+        const data = req.body;
+        const program = programsControllers.getProgramById(program_id);
+
+        if (!Object.keys(data).length && !req.file) throw { message: 'Missing data', status: 400 };
+
+        if (!data.chapter_num || !req.file) {
+            throw {
+                message: "All fields must be completed",
+                fields: {
+                    chapter_num: "number",
+                    chapter_video: "video_file.mp4"
+                },
+                status: 400
+            }
+        };
+
+        // Tratamiento datos
+        data.chapter_num = +data.chapter_num;
+        data.url = `${req.hostname}:8000/uploads/anime/chapters/${req.file.filename}`;
+
+        const newChapter = chaptersControllers.createChapter(data, program_id);
+        res.status(201).json(newChapter);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message, fields: error.fields || null });
+    }
+};
+
+const getOne = (req, res) => {
+    try {
+        const { program_id, chapter_id } = req.params;
+        const program = programsControllers.getProgramById(program_id);
+        const chapter = chaptersControllers.getChapterById(chapter_id);
+
+        res.status(200).json(chapter);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message });
+    }
+};
+
+const edit = (req, res) => {
+    try {
+        const { program_id, chapter_id } = req.params;
+        const data = req.body;
+        const program = programsControllers.getProgramById(program_id);
+
+        if (!data.chapter_num && !req.file) {
+            throw {
+                message: "All fields must be completed",
+                fields: {
+                    chapter_num: "number",
+                    chapter_video: "video_file.mp4"
+                },
+                status: 400
+            }
+        };
+
+        // Tratamiento de datos
+        data.chapter_num = data.chapter_num && +data.chapter_num;
+        data.url = data.url && `${req.hostname}:8000/uploads/anime/chapters/${req.file.filename}`;
+
+        const editedChapter = chaptersControllers.editChapter(chapter_id, data);
+        res.status(200).json({ message: "Chapter was succesfully edited", editedChapter });
+
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message, fields: error.fields || null });
+    }
+};
+
+const remove = (req, res) => {
+    try {
+        const { program_id, chapter_id } = req.params;
+        const program = programsControllers.getProgramById(program_id);
+        chaptersControllers.deleteChapter(chapter_id);
+
+        res.status(204).json({});
+    } catch (error) {
+        res.status(error.status || 500).json({message: error.message});
+    }
+};
+
+module.exports = {
+    getChapters,
+    create,
+    getOne,
+    edit,
+    remove
+}
