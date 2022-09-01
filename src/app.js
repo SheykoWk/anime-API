931bb880bdcbe8b11595396862f6ede7ae8a2c6f
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-08-31 19:59:25 -0500

    Olvide agregar las ruta para ver los archivos de media

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 //* Dependencias
 const express = require("express");
 const passport = require("passport");
-const { uploadCovers, uploadChapters } = require("./utils/multer");
+const path = require('path');
+const fs = require('fs');
 require("./middleware/auth.middleware")(passport);
 
 //*Archivos de rutas
 const userRouter = require("./users/users.router").router;
 const authRouter = require("./auth/auth.router").router;
 const programsRouter = require("./programs/programs.router").router;
+
+// Multer
+const { uploadCovers, uploadChapters } = require("./utils/multer");
 
 //* Configuraciones iniciales
 const app = express();
@@ -21,20 +25,37 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "All ok!" });
 });
 
-app.post('/prueba-covers', uploadCovers.single('cover-img'), (req, res) => {
-  const { title, description } = req.body;
-  res.status(200).json({ message: req.file.filename, title, description });
-});
-
-app.post('/prueba-chapters', uploadChapters.single('chapter-video'), (req, res) => {
-  const { program_id, chapter_num } = req.body;
-  res.status(200).json({ message: req.file.filename, program_id, chapter_num });
-});
 
 app.use("/api/v1", router);
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/programs", programsRouter);
+
+app.get('/uploads/animes/:id_cover', (req, res) => {
+  try {
+    const { id_cover } = req.params;
+    const coverPath = path.join(__dirname, '../media/covers', id_cover);
+
+    if (!fs.existsSync(coverPath)) throw { message: "Image don't found", status: 404 };
+
+    res.status(200).sendFile(coverPath);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+});
+
+app.get('/uploads/anime/chapters/:id_chapter', (req, res) => {
+  try {
+    const { id_chapter } = req.params;
+    const chapterPath = path.join(__dirname, '../media/chapters', id_chapter);
+
+    if (!fs.existsSync(chapterPath)) throw { message: "Video don't found", status: 404 };
+
+    res.status(200).sendFile(chapterPath);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+});
 
 app.listen(8000, () => {
   console.log("Server started at port 8000");
